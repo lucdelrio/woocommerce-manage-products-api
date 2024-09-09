a27e108defceccc95182cc18f62ddbe034ba6df7
Lucas del Rio <lucasgdelrio93@gmail.com>
2024-09-09 16:32:18 -0300

    Add timeout to Httparty Get Product

--- app/services/zecat_chile_api/products.rb
+++ app/services/zecat_chile_api/products.rb
@@ -5,26 +5,26 @@ module ZecatChileApi
     class << self
       def get_generic_product
         url = "#{ZECAT_ENDPOINT}/generic_product"
-        response = HTTParty.get(url)
+        response = HTTParty.get(url, timeout: 120)
         JSON.parse(response.body)
       end
 
       def generic_product_by_id(product_id)
         url = "#{ZECAT_ENDPOINT}/generic_product/#{product_id}"
-        response = HTTParty.get(url)
+        response = HTTParty.get(url, timeout: 120)
         JSON.parse(response.body)
       end
 
       def get_generic_product_by_page_with_sort(sort_type, sort, page_number, limit)
         url = "#{ZECAT_ENDPOINT}/generic_product?order[#{sort_type}]=#{sort}&page=#{page_number}&limit=#{limit}"
-        response = HTTParty.get(url)
+        response = HTTParty.get(url, timeout: 120)
 
         JSON.parse(response.body)
       end
 
       def get_generic_product_by_page(page_number, limit)
         url = "#{ZECAT_ENDPOINT}/generic_product?page=#{page_number}&limit=#{limit}"
-        response = HTTParty.get(url)
+        response = HTTParty.get(url, timeout: 120)
 
         JSON.parse(response.body)
       end
@@ -36,7 +36,7 @@ module ZecatChileApi
       # ZecatChileApi::Products.get_generic_product_by_family_with_pages('128', '1', '5')
       def get_generic_product_by_family_with_pages(family_id, page_number, limit)
         url = "#{ZECAT_ENDPOINT}/generic_product?families[]=#{family_id}&page=#{page_number}&limit=#{limit}"
-        response = HTTParty.get(url)
+        response = HTTParty.get(url, timeout: 120)
 
         JSON.parse(response.body)
       end
