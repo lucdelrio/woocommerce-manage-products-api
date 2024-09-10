5bf6bd17cc1bd21d9e7df31c76e251a74e71e85b
Lucas del Rio <lucasgdelrio93@gmail.com>
2024-09-10 11:14:20 -0300

    Delete product when zecat_product is not found (#9)

--- app/services/integration/tools/products_cleanup.rb
+++ app/services/integration/tools/products_cleanup.rb
@@ -7,16 +7,16 @@ module Integration
         def iterate_products_and_destroy(zecat_country)
           Product.all.each_slice(10) do |group|
             group.each do |local_product|
-              zecat_product = CountrySelection::zecat_class_name(zecat_country)::Products.generic_product_by_id(local_product.zecat_id)
+              zecat_product = CountrySelection::zecat_class_name(zecat_country)::Products.generic_product_by_id(local_product.zecat_id).dig('internal_code')
               woocommerce_product = CountrySelection::woocommerce_class_name(zecat_country).product_by_id(local_product.woocommerce_api_id)
 
               (local_product.destroy! ; next) unless local_product.woocommerce_api_id.present?
 
-              next if zecat_product.present? && woocommerce_product.present?
+              next if (zecat_product != 'not_found') && woocommerce_product.present?
 
-              if zecat_product.present? && !woocommerce_product.present?
+              if (zecat_product != 'not_found') && !woocommerce_product.present?
                 local_product.update(last_sync: nil)
-              elsif !zecat_product.present?
+              elsif zecat_product == 'not_found'
                 local_product.destroy!
               end
             end
