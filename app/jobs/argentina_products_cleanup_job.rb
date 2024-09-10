5ca608dc10cc779a5de795bf4a9198fbb051d723
Lucas del Rio <lucasgdelrio93@gmail.com>
2024-09-10 12:23:42 -0300

    Refactor products cleanup (#10)

--- app/jobs/argentina_products_cleanup_job.rb
+++ app/jobs/argentina_products_cleanup_job.rb
@@ -6,6 +6,6 @@ class ArgentinaProductsCleanupJob
   sidekiq_options retry: false
 
   def perform
-    # Integration::Tools::ProductsCleanup.iterate_products_and_destroy('Argentina')
+    Integration::Tools::ProductsCleanup.iterate_products_and_destroy('Argentina')
   end
 end
