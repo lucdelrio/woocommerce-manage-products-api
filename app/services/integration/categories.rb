ced0881cb1c4d90406c0aa46f4aa56b057b9ddfe
Lucas del Rio <lucasgdelrio93@gmail.com>
2025-04-28 09:37:40 -0300

    Avoid creating inactive category

--- app/services/integration/categories.rb
+++ app/services/integration/categories.rb
@@ -4,12 +4,14 @@ module Integration
   class Categories
     def initialize(zecat_country)
       @zecat_country = zecat_country
-      @zecat_categories = CountrySelection::zecat_class_name(zecat_country)::Families.categories
+      @zecat_categories = CountrySelection.zecat_class_name(zecat_country)::Families.categories
     end
 
     def iterate_categories_and_sync
       @zecat_categories.each do |category|
         category_hash = EntityGeneration::Categories.category_hash(category)
+        next if category['show'] == false
+
         local_category = find_or_create_local_category(category['id'], category_hash)
 
         sync_category(local_category, category_hash)
@@ -20,10 +22,12 @@ module Integration
       local_category.update(last_sync: nil) unless remote_available(local_category)
 
       if local_category.woocommerce_api_id.nil?
-        woocommerce_category = CountrySelection::woocommerce_class_name(@zecat_country).create_category(category_hash)
+        woocommerce_category = CountrySelection.woocommerce_class_name(@zecat_country).create_category(category_hash)
         sync_local(local_category, woocommerce_category, category_hash)
       elsif category_hash.to_json != local_category.category_hash.to_json
-        woocommerce_category = CountrySelection::woocommerce_class_name(@zecat_country).update_category(local_category.woocommerce_api_id, category_hash)
+        woocommerce_category = CountrySelection.woocommerce_class_name(@zecat_country).update_category(
+          local_category.woocommerce_api_id, category_hash
+        )
         sync_local(local_category, woocommerce_category, category_hash)
       end
     end
@@ -31,21 +35,25 @@ module Integration
     def remote_available(local_category)
       return false if local_category.woocommerce_api_id.nil?
 
-      !CountrySelection::woocommerce_class_name(@zecat_country).category_by_id(local_category.woocommerce_api_id).nil?
+      !CountrySelection.woocommerce_class_name(@zecat_country).category_by_id(local_category.woocommerce_api_id).nil?
     end
 
     def sync_local(local_category, woocommerce_category, category_hash)
       woocommerce_category_id = if woocommerce_category.try(:success?)
-                                  woocommerce_category.dig('id')
+                                  woocommerce_category['id']
                                 else
-                                  JSON.parse(woocommerce_category.body).dig('data','resource_id') || woocommerce_category.dig('id')
+                                  JSON.parse(woocommerce_category.body).dig('data',
+                                                                            'resource_id') || woocommerce_category['id']
                                 end
 
-      (local_category.destroy! ; return) unless woocommerce_category_id.present?
+      unless woocommerce_category_id.present?
+        (local_category.destroy!
+         return)
+      end
 
       local_category.update(woocommerce_api_id: woocommerce_category_id.to_s,
-                              woocommerce_last_updated_at: Time.zone.now,
-                              last_sync: Time.zone.now, category_hash: category_hash)
+                            woocommerce_last_updated_at: Time.zone.now,
+                            last_sync: Time.zone.now, category_hash: category_hash)
     end
 
     def find_or_create_local_category(zecat_id, category_hash)
